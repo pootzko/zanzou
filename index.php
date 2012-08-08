2f35269b3d6dd075fae60422bff97cc493b02f40
pootzko <pootzko@gmail.com>
2012-08-08 23:05:45 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,86 @@
+<?php /******************************************************
+*===========================================================*
+*         - zanzou -                                        *
+*===========================================================*
+*************************************************************
+*
+* Copyright 2012, Tihomir Kit (kittihomir@gmail.com)
+* spilp is distributed under the terms of GNU General Public License v3
+* A copy of GNU GPL v3 license can be found in LICENSE.txt or
+* at http://www.gnu.org/licenses/gpl-3.0.html
+*
+********************************************************/ ?>
+
+<!DOCTYPE HTML>
+<html>
+<head>
+	<META http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<title>Memorize kana easily with Zanzou</title>
+	<link rel="stylesheet" type="text/css" href="zanzou.css" />
+	<link rel="shortcut icon" href="/images/favicon.ico" />
+	<script type="text/javascript">
+		var _gaq = _gaq || [];
+		_gaq.push(['_setAccount', 'UA-11297782-1']);
+		_gaq.push(['_setDomainName', 'cmikavac.net']);
+		_gaq.push(['_trackPageview']);
+
+		(function() {
+			var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+			ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+			var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+		})();
+	</script>
+</head>
+
+
+
+
+<body>
+<div id="header"> <!-- HEADER -->
+	<div id="logo_holder">
+		<div id="header_kanji">残像</div>
+		<div id="header_latin">memorize kana easily with Zanzou</div>
+	</div>
+</div> <!-- /HEADER -->
+
+
+
+<div id="wrapper"> <!-- WRAPPER -->
+		<div id="navigation"> <!-- NAVIGATION -->
+			<ul>
+				<li><a href="/">home</a></li>
+				<li><a href="/">options</a></li>
+				<li><a href="/">about</a></li>
+				<li><a href="/">practise</a></li>
+				<li><a href="/">stats</a></li>
+				<li><a href="/">about</a></li>
+			</ul>
+		</div> <!-- /NAVIGATION -->
+
+
+		<div id="content_holder"> <!-- CONTENT_HOLDER -->
+			<div id="content"> <!-- CONTENT -->
+				<h2>Home</h2>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+			</div> <!-- /CONTENT -->
+
+
+			<div id="footer"> <!-- FOOTER -->
+				<b>::::</b> <a href="http://cmikavac.net">cmikavac.net</a> <b>--</b> <a href="mailto:kittihomir@gmail.com">kittihomir@gmail.com</a>
+			</div> <!-- /FOOTER -->
+		</div> <!-- /CONTENT_HOLDER -->
+</div> <!-- /WRAPPER -->
+</body>
+</html>
