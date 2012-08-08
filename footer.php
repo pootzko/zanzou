429dd5528cbe2ec8254835bb3f2bd75bf61350e3
pootzko <pootzko@gmail.com>
2012-08-08 23:26:54 +0200

    initialized web template

---
+++ footer.php
@@ -0,0 +1,7 @@
+		<div id="footer"> <!-- FOOTER -->
+			<b>::::</b> <a href="http://cmikavac.net">cmikavac.net</a> <b>--</b> <a href="mailto:kittihomir@gmail.com">kittihomir@gmail.com</a>
+		</div> <!-- /FOOTER -->
+	</div> <!-- /CONTENT_HOLDER -->
+</div> <!-- /WRAPPER -->
+</body>
+</html>
---
+++ navigation.php
@@ -0,0 +1,13 @@
+	<div id="navigation"> <!-- NAVIGATION -->
+		<ul>
+			<li><a href="/">home</a></li>
+			<li><a href="/">options</a></li>
+			<li><a href="/">about</a></li>
+			<li><a href="/">practise</a></li>
+			<li><a href="/">stats</a></li>
+			<li><a href="/">about</a></li>
+		</ul>
+	</div> <!-- /NAVIGATION -->
+
+
+	<div id="content_holder"> <!-- CONTENT_HOLDER -->
