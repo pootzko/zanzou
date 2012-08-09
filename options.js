f19bf00fcca6c3a5b72b1107e8efbe3285874453
pootzko <pootzko@gmail.com>
2012-08-09 11:24:08 +0200

    completed kana array sets

--- options.js
+++ options.js
@@ -5,7 +5,10 @@ function generateTable() {
 		table_content += "<tr>";
 
 		for(j=0; j<5; j++) {
-			table_content += "<td>" + hiragana_monograhps[i][j] + "</td>";
+			table_content += "<td>";
+			if (monographs[i][j][1] != "")
+				table_content += monographs[i][j][1] + " (" + monographs[i][j][0] + ")";
+			table_content += "</td>";
 		}
 
 		table_content += "</tr>";
