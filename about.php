30b128a5ab708560310d000db38fa34a4b60f501
pootzko <pootzko@gmail.com>
2012-08-31 15:23:29 +0200

    prevent same flashcard from appearing a few times in a row and modified descriptions

---
+++ about.php
@@ -0,0 +1,37 @@
+<?php require 'head.php'; ?>
+
+<script type="text/javascript" src="javascript_libs/jquery-1.7.2.min.js"></script>
+<script type="text/javascript" src="javascript_libs/jquery.cookie.js"></script>
+<script type="text/javascript" src="cookie.js"></script>
+
+<?php
+require 'header.php';
+require 'navigation.php';
+?>
+
+
+
+<h2>About</h2>
+<div id="content"> <!-- CONTENT -->
+	<h3>About Zanzou</h3>
+	<p>Zanzou is an e-learning computer science master's degree thesis project at <a href="http://www.etfos.unios.hr/">Faculty of electrical engineering</a>, Osijek, Croatia. Its goal is to help people memorize kana using flashcards. Based on (un)successful answering it will repeat certain symbols more frequently and depending on the chosen difficulty level, it will group and present some of the similar symbols together to make it harder to "guess the answer". Global user success statistics are also available.</p><br />
+
+	<h3>Thanks to</h3>
+	<ul>
+		<li>Gerd Tentlers <a href="http://www.gerd-tentler.de/tools/">tooltip JavaScript lib</a></li>
+		<li>Klaus Hartls <a href="https://github.com/carhartl/jquery-cookie">jquery-cookie plugin</a></li>
+		<li>Koichi from <a href="http://www.textfugu.com/">textfugu</a> for generously providing me with a set of kana sound samples</li>
+		<li>my mentor <a href="http://www.etfos.unios.hr/~martin/?tekst=info">Goran Martinović</a>, PhD</li>
+		<li>and to all my friends who helped me test Zanzou and provided me with feedback</li>
+	</ul><br />
+
+	<p>For any feedback or bug reports, please contact me through the email provided at the bottom of the page.</p>
+</div> <!-- /CONTENT -->
+
+
+
+<?php require 'footer.php'; ?>
+
+
+
+
